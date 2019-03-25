c054ffdac38135449a43371b0ec4c930d583951f
Kevin Clement <kevin.clement@gmail.com>
2019-03-25 00:50:20 -0700

    remove hello world stuff

---
+++ src/views/Quiz.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="quiz">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quiz',
+  props: {
+  },
+  data() {
+    return {
+      msg: "Hello quiz"
+    }
+  },
+}
+</script>
+
+<style scoped>
+</style>
