04946df137e6ab15ffcd3490a7ae9d06401f2947
Kevin Clement <kevin.clement@gmail.com>
2019-04-03 02:08:52 -0700

    add some sounds

--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -18,6 +18,9 @@
           </div>
           <div class="progress">Question {{questionIndex + 1}}/{{QUESTION_LIMIT}}</div>
         </div>
+        <audio autoplay="autoplay" v-if="this.timeForSelection">
+          <source src="../assets/sounds/clock-ticking-4.wav" type="audio/wav">
+        </audio>
       </div>
     </div>
 
