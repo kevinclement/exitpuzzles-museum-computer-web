2ace3fd29c2f5856cff8d42de89efeff743cdb01
Kevin Clement <kevin.clement@gmail.com>
2019-10-04 20:44:40 -0700

    updated final journal for real puzzle

--- src/views/Journal.vue
+++ src/views/Journal.vue
@@ -3,42 +3,37 @@
   <div class="journal">
     <div style="padding-top:25px">
       <pre style="font-family: 'Press Start 2P';">
-  .------------------------------------------------.
- /  .-.                                        .-.  \
-|  /   \   My Journal                         /   \  |
-| |\_.  |                                    |    /| |
-|\|  | /|                                    |\  | |/|
-| `---' |  19 Oct 1987                       | `---' |
-|       |                                    |       |
-|       |                                    |       |
-|       |  Last night was sunday, so i was   |       |
-|       |  at my mums for the weekend. i     |       |
-|       |  woke up at 5:50 am, i thought i   |       |
-|       |  heard light taping on my bedroom  |       |
-|       |  door after i looked at the time   |       |
-|       |  on my mobile phone however i      |       |
-|       |  down to sleep some more before i  |       |
-|       |  had to i soon thought i heard the |       |
-|       |  knocking again, so i sat up and   |       |
-|       |  looked at the door, but nothing   |       |
-|       |  else came so i went back to       |       |
-|       |  sleep.                            |       |
-|       |                                    |       |
-|       |                                    |       |
-|       |  I woke up at 8:05 and had a       |       |
-|       |  showerdad arrived while i was     |       |
-|       |  packing, and we soon left for     |       |
-|       |  work.                             |       |
-|       |                                    |       |
-|       |                                    |       |
-|       |                                    |       |
-|       |                                    |       |
-|       |                                    |       |
-|       |                                    |       |
-|       |------------------------------------|       |
-\       |                                    |       /
- \     /                                      \     /
-  `---'                                        `---'
+  .--------------------------------------------------.
+ /  .-.                                          .-.  \
+|  /   \                                        /   \  |
+| |\_.  |  My Journal                          |    /| |
+|\|  | /|                                      |\  | |/|
+| `---' |                                      | `---' |
+|       |                                      |       |
+|       |  19 Oct 1987                         |       |
+|       |                                      |       |
+|       |                                      |       |
+|       |                                      |       |
+|       |  i Seriously tHink theRe may be      |       |
+|       |  somethIng to this egyptiaN cursE.   |       |
+|       |  my luck has hapPened to chAnge ever |       |
+|       |  Since that Shipment from cairo      |       |
+|       |  arrived.                            |       |
+|       |                                      |       |
+|       |                                      |       |
+|       |                                      |       |
+|       |  i noW believe the Only way to turn  |       |
+|       |  everything aRound and enD thIs      |       |
+|       |  curse, iS to return All five idoLs   |       |
+|       |  to Their shrine, and remember to    |       |
+|       |  plAce them in the correct ordeR.    |       |
+|       |                                      |       |
+|       |                                      |       |
+|       |                                      |       |
+|       |--------------------------------------|       |
+\       |                                      |       /
+ \     /                                        \     /
+  `---'                                          `---'
       </pre>
 
     </div>
@@ -77,5 +72,6 @@ export default {
     color: #00D46A;
     font-family: 'Press Start 2P';
     font-size: 18px;
+    line-height: 22px;
   }
 </style>
